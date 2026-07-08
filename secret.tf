8c443704b251bb7d6d32d6ed76f06a7aba779942
Kasun Vithanage <nvkasun@gmail.com>
2026-07-08 09:26:09 +0400

    added new secret for target pod

--- secret.tf
+++ secret.tf
@@ -15,3 +15,40 @@ module "secret_protected" {
   data_classification  = "General"
   env                  = "dev"
 }
+
+
+module "admin_secret_protected" {
+  source = "git::https://github.com/AbuDhabiCommercialBank/aws-tf-module-secrets-manager.git?ref=v2.0.2"
+
+  name                    = "dev/goldengate/admin-secret"
+  description             = "Goldengate admin login secret key"
+  recovery_window_in_days = 0
+  safety_mode             = "protected"
+
+  # Mandatory tags
+  map_migrated         = "comm5TZY31HX9S"
+  business_criticality = "Low"
+  application_name     = "CloudFactory"
+  cost_center          = "219"
+  business_unit        = "TechnologyPlatform"
+  data_classification  = "General"
+  env                  = "dev"
+}
+
+module "admin_secret_target_protected" {
+  source = "git::https://github.com/AbuDhabiCommercialBank/aws-tf-module-secrets-manager.git?ref=v2.0.2"
+
+  name                    = "dev/goldengate/admin-secret-target"
+  description             = "Goldengate admin login secret key"
+  recovery_window_in_days = 0
+  safety_mode             = "protected"
+
+  # Mandatory tags
+  map_migrated         = "comm5TZY31HX9S"
+  business_criticality = "Low"
+  application_name     = "CloudFactory"
+  cost_center          = "219"
+  business_unit        = "TechnologyPlatform"
+  data_classification  = "General"
+  env                  = "dev"
+}
