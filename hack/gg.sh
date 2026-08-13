70cf389a0f511967377e08a691a5d14ce57d24a8
Kasun Vithanage <nvkasun@gmail.com>
2026-08-13 19:20:01 +0400

    dfsd

--- hack/gg.sh
+++ hack/gg.sh
@@ -1,1 +1,14 @@
-sg-077d0e9e30e5a6cde
+Run:
+
+kubectl patch ingress gg-monitor \
+  -n goldengate-monitoring \
+  --type=merge \
+  -p '{"metadata":{"finalizers":[]}}'
+
+Then:
+
+kubectl get ingress -A
+
+Expected:
+
+No resources found
